bd8dd6d41a51fb32a00c24aedab33829b4b209ee
internet-polluter <internet0polluter@gmail.com>
2025-12-03 22:32:17 -0700

    begin first script

--- playwright.ts
+++ playwright.ts
@@ -4,7 +4,7 @@ async function main() {
   const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto("https://google.com");
+  await page.goto("https://discord.com");
 
   await browser.close();
 }
