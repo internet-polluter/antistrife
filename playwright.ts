9f2236593a054b8eb0feb2e2c8fa5e95f27c6ea8
internet-polluter <internet0polluter@gmail.com>
2025-12-03 19:18:08 -0700

    cleanup test script

--- playwright.ts
+++ playwright.ts
@@ -1,20 +1,12 @@
 import { chromium } from "playwright";
 
 async function main() {
-  console.log("Script starting...");
   const browser = await chromium.launch({ headless: false });
-  console.log("Browser launched");
   const page = await browser.newPage();
-  console.log("Page created");
 
   await page.goto("https://google.com");
-  console.log("Navigated to Google");
-  await page.fill('input[name="q"]', "hello world");
-  console.log("Text typed");
-  await page.waitForTimeout(10000);
 
   await browser.close();
-  console.log("Done");
 }
 
 main();
